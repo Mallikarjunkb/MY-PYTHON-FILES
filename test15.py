814dd80e4b5cc0d77dcfb2778f618c87de18c7ec
Mallikarjunkb <31765679+Mallikarjunkb@users.noreply.github.com>
2017-09-14 15:34:22 +0530

    Add files via upload

---
+++ test15.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+name="cmr"
+print(name)
+print(name*5)
---
+++ test16.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# guess what this program does ???
+import  random
+r=random.randidant(23,49)
+print(r)
