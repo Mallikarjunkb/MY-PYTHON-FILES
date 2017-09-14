6962b4f67b6108513e914853809ef27ce854f69e
Mallikarjunkb <31765679+Mallikarjunkb@users.noreply.github.com>
2017-09-14 15:32:48 +0530

    Add files via upload

---
+++ test19.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+for i in range(2,20):
+	if(i%2==0):
+		print(i)
+for i in range(3,30):
+	if(i%3==0):
+		print(i)
+for i in range(4,40):
+	if(i%4==0):
+		print(i)
+		for i in range(5,50):
+			if(i/5==0):
+				print(i)
