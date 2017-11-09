4f56387efea21c7b3519547f88cdf7e009ec856d
Mallikarjunkb <31765679+Mallikarjunkb@users.noreply.github.com>
2017-11-09 15:11:53 +0530

    Update test10.py

--- test10.py
+++ test10.py
@@ -11,20 +11,21 @@ print("2 paper")
 print ("3 scissors")
 
 player=input("choose from 1-3:")
+# choosing player 1
 if player==1:
 	print("you choose Rock")
 	sleep(2)
 	print("CPU chooses paper")
 	sleep(.5)
 	print("you lose, and you will never win!")
-
+# choosing player 2
 elif player==2:
 	print("you choose paper")
 	sleep(2)
 	print("CPU chooses scissors")
 	sleep(.5)
 	print("you lose, and ypu will never win!")
-
+# choosing player 3
 else:
 	print("you choose scissors")
 	sleep(2)
