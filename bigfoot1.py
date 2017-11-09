67bfcbbcb834a879ad9290ec878baebd3df3cef1
Mallikarjunkb <31765679+Mallikarjunkb@users.noreply.github.com>
2017-11-09 15:30:44 +0530

    Update bigfoot1.py

--- bigfoot1.py
+++ bigfoot1.py
@@ -1,3 +1,4 @@
+# program to take a photo using raspberry pi
 Python 3.5.3 (default, Jan 19 2017, 14:11:04) 
 [GCC 6.3.0 20170124] on linux
 Type "copyright", "credits" or "license()" for more information.
