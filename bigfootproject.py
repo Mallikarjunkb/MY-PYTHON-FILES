bc3d71b2f315de6bda62fe84084a050634268a6a
Mallikarjunkb <31765679+Mallikarjunkb@users.noreply.github.com>
2017-11-09 15:33:09 +0530

    Update bigfootproject.py

--- bigfootproject.py
+++ bigfootproject.py
@@ -1,4 +1,4 @@
-# programing of upload photo from raspberry pi
+# programing for pic a photo using raspberry pi
 Python 3.5.3 (default, Jan 19 2017, 14:11:04) 
 [GCC 6.3.0 20170124] on linux
 Type "copyright", "credits" or "license()" for more information.
