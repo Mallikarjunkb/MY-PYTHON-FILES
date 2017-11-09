da77a4b17ecff643851562a17d22b403a2fd830b
Mallikarjunkb <31765679+Mallikarjunkb@users.noreply.github.com>
2017-11-09 15:00:48 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import random 
 r=random.randint(1,6)
+# possibilities are 1 to 6
 print (r)
 #prints r
 if r==6:
  	print (r)
-  #prints r
+#prints r
  	print ("exellent")
-  #print exellent
+#print exellent
