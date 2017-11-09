e5e9e0d4f1d8c8643b7907a4d21409e2743d29ab
Mallikarjunkb <31765679+Mallikarjunkb@users.noreply.github.com>
2017-11-09 21:50:45 +0530

    Update test22.py

--- test22.py
+++ test22.py
@@ -1,7 +1,7 @@
-# finding the vavlue o i 
+# finding the vavlue of i 
 #!/usr/bin/python3
 for i in range(2,20):
 # i is only in the range 2 to 2o
 	if(i%2==0):
 		print(i)
-#prints i		
+# prints i		
