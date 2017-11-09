c8d05793a532d5b8175ae9f367b3a2312f3602cb
Mallikarjunkb <31765679+Mallikarjunkb@users.noreply.github.com>
2017-11-09 14:17:43 +0530

    Update test19.py

--- test19.py
+++ test19.py
@@ -1,13 +1,21 @@
 #!/usr/bin/python3
 for i in range(2,20):
+# i is only in the range 2 to 20
 	if(i%2==0):
 		print(i)
+# prints i
 for i in range(3,30):
+# i is only in the range 3 to 30
 	if(i%3==0):
 		print(i)
+# printss i
 for i in range(4,40):
+# i is only in the range of 4 to 40
 	if(i%4==0):
 		print(i)
+# it prints i
 		for i in range(5,50):
+# is only in the range 5 to 50
 			if(i/5==0):
 				print(i)
+# it prints i
